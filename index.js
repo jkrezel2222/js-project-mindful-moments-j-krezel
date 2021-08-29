01a024bed9d3d59e2feb252d555745de2ea8eb83
Jannette <jannette.krezel@gmail.com>
2021-08-29 12:53:43 +1000

    adding fetch, testing fetching data

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
-window.addEventListener("load", function () {
+// window.addEventListener("load", function () {
 
-    fetch("https://quote-garden.herokuapp.com/api/v3/quotes/").then(function(response) {
+//     fetch("https://quote-garden.herokuapp.com/api/v3/quotes/").then(function(response) {
+//         console.log("Content-type: ", response.headers.get("Content-Type"));
+//         console.log("Redirected: ", response.redirected);
+//         console.log("Status: ", response.status);
+//         console.log("Status-text: ", response.statusText);
+//         console.log("Response-type: ", response.type);
+//         console.log("Response-URL: ", response.url);
+//         console.log("Response-headers: ", response.headers);
+
+// // equal to 200 to show there are no errors and status is ok
+//          if (response.status == 200) {
+//              return response.json();
+//          }
+         
+//     }).then(function(returnedData) {
+//         console.log("Returned data: ", returnedData);
+//     }).catch(function (error) {
+//      console.log("Error: ", error);
+// })
+
+
+function fetchData() {
+    fetch("https://quote-garden.herokuapp.com/api/v3/quotes")
+    .then(response => {
         console.log("Content-type: ", response.headers.get("Content-Type"));
         console.log("Redirected: ", response.redirected);
         console.log("Status: ", response.status);
         console.log("Status-text: ", response.statusText);
         console.log("Response-type: ", response.type);
         console.log("Response-URL: ", response.url);
-        console.log("Response-headers: ", response.headers);
-
-        //headers "pagination.currentPage: x", "_id" eg "5eb17aadb69dc744b4e70d2e", "quoteText", "quoteAuthor"
-
-        //equal to 200 to show there are no errors and status is ok
-         if (response.status == 200) {
-             return response.json();
-         }
-         
-    }).then(function(returnedData) {
-        console.log("Returned data: ", returnedData);
-    }).catch(function (error) {
-     console.log("Error: ", error);
+        console.log("Response-headers: ", response.headers);    
+        if(!response.ok) {
+            throw Error("Error");
+        }
+        return response.json();
     })
+    .then(data => {
+        const html = data.data
+        .map(data => {
+            return `
+            <div class="data">
+            <p>${data.quoteText}</p>
+            </div>
+            `;
+        })
+        .join("");
+        document.querySelector("#fromAPI").insertAdjacentHTML("afterbegin", html);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+fetchData();
 
 
-})
+//use the reduce() method to process my data array
--- practice.js
+++ practice.js
@@ -1,6 +1,5 @@
 
         /*window.addEventListener("load", function () {
-            //TODO add my API address here *****
            fetch("http://api.forismatic.com/api/1.0/").then(function(response) {
                console.log("Content-type: ", response.headers.get("Content-Type"));
                console.log("Redirected: ", response.redirected);
@@ -33,3 +32,32 @@
                 })
 
            });
+
+
+           // let response = fetch("https://quote-garden.herokuapp.com/api/v3/quotes/");
+
+// fetch("https://quote-garden.herokuapp.com/api/v3/quotes/")
+// .then(response => {
+//     return response.json();
+// })
+// .then
+
+// .catch(function(error) {
+//     console.log("Error: ", error);
+// })
+
+
+
+
+//         //headers "pagination.currentPage: x", "_id" eg "5eb17aadb69dc744b4e70d2e", "quoteText", "quoteAuthor"
+
+// nestedObject[keyVar]
+
+// fetch("https://quote-garden.herokuapp.com/api/v3/quotes/", {
+//     method: "POST",
+//     headers: {
+//         //headers here
+//     }
+//     return array and store as "myQuotes"
+//     myQuotes = [array in here]
+// })
